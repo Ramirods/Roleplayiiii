5e12e40f37865ac5577c563796686aaada679b76
Ramirods <ramiro.dasilva@correo.ucu.edu.uy>
2023-05-28 21:43:24 -0300

    Itemcompuesto

---
+++ src/Library/items/Armor.cs
@@ -0,0 +1,16 @@
+namespace RoleplayGame.Items
+{
+public class Armor: IDefenseItem
+    {
+        public Helmet helmet = new Helmet();
+        public Robes robes = new Robes();
+
+        public int DefensePower
+        {
+            get
+            {
+                return this.helmet.DefensePower + this.robes.DefensePower
+            }
+        }    
+    }
+}
